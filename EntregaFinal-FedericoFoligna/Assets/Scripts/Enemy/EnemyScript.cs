d0953d27bb5376a4748927496eb0ff3f0e35341d
Federico Foligna <ffoligna@users.noreply.github.com>
2022-11-12 14:01:29 -0300

    v 0.0.4
    Implementación de música y efectos de sonido.

---
+++ EntregaFinal-FedericoFoligna/Assets/Scripts/Enemy/EnemyScript.cs
@@ -0,0 +1,25 @@
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyScript : MonoBehaviour
+{
+
+    public Transform AmyNavMesh;
+    private NavMeshAgent enemy;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemy = GetComponent<NavMeshAgent>();    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        enemy.destination = AmyNavMesh.position;
+    }
+
+}
